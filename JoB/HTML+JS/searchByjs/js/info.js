541f0ec5fb020d2f9fbd9c9f05726083c3567d45
WatcharapolxFa <watcharapolyotadee@outlook.com>
2022-11-18 21:09:54 +0700

    Source

--- JoB/HTML+JS/searchByjs/js/info.js
+++ JoB/HTML+JS/searchByjs/js/info.js
@@ -1,3 +1,22 @@
+const allStudents = [
+    { 
+        "id": 1, 
+        "name": "aisikawadee", 
+        "year": 2003, 
+        "user": "AisikawadeeRat", 
+        "grade": "3.11", 
+        "quote": "earth without art is eh"  
+    },
+    { 
+        "id": 2, 
+        "name": "gronwannapairote", 
+        "year" : 2002, 
+        "user" : "GronProxie",
+        "grade" : "3.97", 
+        "quote ": "silent mode" 
+    }
+]
+
 function getParameterByName(name, url = window.location.href) {
     name = name.replace(/[\[\]]/g, '\\$&');
     var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
@@ -6,10 +25,31 @@ function getParameterByName(name, url = window.location.href) {
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
-let id ; // get value from param 'id'
 
 function loadDoc(){
 	// find student by id and show student detail
 	
 	
 }
+
+function filterStudent(id) {
+    for (let index = 0; index < allStudents.length; index++) {
+        if (allStudents[index].id === id) {
+            return allStudents[index]
+        }
+    }
+}
+
+function getParams() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const id = parseInt(urlParams.get("id"))
+    const filteredStudent = filterStudent(id)
+    document.getElementById("name").innerHTML += filteredStudent.name
+    document.getElementById("year").innerHTML += filteredStudent.year
+    document.getElementById("user").innerHTML += filteredStudent.user
+    document.getElementById("grade").innerHTML += filteredStudent.grade
+    document.getElementById("quote").innerHTML += filteredStudent.quote
+}
+
+getParams();
--- JoB/HTML+JS/searchByjs/js/search.js
+++ JoB/HTML+JS/searchByjs/js/search.js
@@ -1,25 +1,54 @@
+const allStudents = [
+    { 
+        "id": 1, 
+        "name": "aisikawadee", 
+        "year": 2003, 
+        "user": "aisikawadeerat", 
+        "grade": "3.11", 
+        "quote": "earth without art is eh" 
+    },
+    { 
+        "id": 2, 
+        "name": "gronwannapairote", 
+        "year" : 2002, 
+        "user" : "GronProxie",
+        "grade" : "3.97", 
+        "quote ": "silent mode" 
+    }
+]
+
+function filterText(name, year) {
+    if (/[1-9]/.test(name)) return
+    let studentFiltered = []
+    allStudents.forEach(val => {
+        if (val.name.toLowerCase().indexOf(name.toLowerCase()) >= 0 && year === val.year) {
+            studentFiltered.push(val)
+        }
+    })
+    return studentFiltered;
+}
+
+function onLiClick(id) {
+    window.location.href = `info.html?id=${id}`
+}
+
 function search(){
+    console.log(allStudents)
 	let searchName = document.getElementById("searchName").value;
 	let searchYear = parseInt(document.getElementById("searchYear").value);
-	// set url to search from input
-}
-
-function getParameterByName(name, url = window.location.href) {
-    name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
-
-let name ; // get value from param 'name'
-let year ; // get value from param 'year'
-
-
-
-function loadDoc(){
-	// search student by name and year param and set hyperlink to info of student
-	
-	
+	let studentList = document.getElementById("studentList")
+    let studentFiltered = filterText(searchName, searchYear)
+    studentFiltered.forEach(val => {
+        let node = document.createElement("li");
+        let textnode = document.createTextNode(`Name: ${val.name} Year: ${val.year}`);
+        let liButton = document.createElement("button")
+        liButton.innerHTML = "view profile"
+        liButton.name = val.name + "Button"
+        liButton.onclick = () => {
+            onLiClick(val.id);
+        }
+        node.appendChild(textnode);
+        node.appendChild(liButton);
+        studentList.appendChild(node);
+    })
 }
